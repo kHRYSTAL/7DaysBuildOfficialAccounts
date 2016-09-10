d1e8f0c78ca359ffa0221e87812124db95331904
kHRYSTAL <723526676@qq.com>
2016-09-11 01:35:47 +0800

    add crawler with promise

--- base2/crawler/promise_crawler.js
+++ base2/crawler/promise_crawler.js
@@ -3,11 +3,13 @@ var Promise = require('bluebird')
 var cheerio = require('cheerio')
 
 var baseUrl = 'http://www.imooc.com/learn/'
-var url = 'http://www.imooc.com/learn/348'
+var videoIds = [75]
 
 function filterChapters(html) {
     var $ = cheerio.load(html)
+    var title = $('.course-infos .path span').text()
 
+    var number = parseInt($($('.meta-value strong')[3]).text().trim(), 0)
     var chapters = $('.chapter')
 
     // [{
@@ -18,7 +20,23 @@ function filterChapters(html) {
     //   ]
     // }]
 
-    var courseData = []
+    // courseData = {
+    //   title: title,
+    //   number: number,
+    //   videos: [{
+    //     chapterTitle:'',
+    //     videos: [
+    //       title:'',
+    //       id:''
+    //     ]
+    //   }]
+    // }
+
+    var courseData = {
+      title: title,
+      number: number,
+      videos: []
+    }
 
     chapters.each(function(item) {
       var chapter = $(this)
@@ -40,24 +58,33 @@ function filterChapters(html) {
           })
       })
 
-    courseData.push(chapterData)
+    courseData.videos.push(chapterData)
     })
 
     return courseData
 }
 
-function printCourseInfo(courseData) {
-  courseData.forEach(function(item) {
-    var chapterTitle = item.chapterTitle
-    console.log(chapterTitle + '\n')
+function printCourseInfo(coursesData) {
+  coursesData.forEach(function(courseData) {
+    console.log(courseData.number + '人学过' + courseData.title + '\n')
+  })
 
-    item.videos.forEach(function(video) {
-      console.log('   ［' + video.id + ']' + video.title + '\n')
+  coursesData.forEach(function(courseData) {
+    console.log('###' + courseData.title.trim() + '\n')
+    courseData.videos.forEach(function(item) {
+      var chapterTitle = item.chapterTitle
+      console.log(chapterTitle + '\n')
+      item.videos.forEach(function(video) {
+        console.log('|' + video.id + '|' + video.title.trim() + '\n')
+      })
     })
   })
 }
 
-function getPageAsync(url) {
+/**
+通过url获取整个页面html代码
+*/
+function getPageDataAsync(url) {
   return new Promise(function(resolve, reject) {
     console.log('正在爬取:' + url)
 
@@ -70,8 +97,6 @@ function getPageAsync(url) {
 
       res.on('end', function() {
         resolve(html)
-        // var courseData = filterChapters(html)
-        // printCourseInfo(courseData)
       })
     }).on('error', function(e) {
       reject(e)
@@ -83,5 +108,30 @@ function getPageAsync(url) {
 var fetchCourseArray = []
 
 videoIds.forEach(function(id) {
-  fetchCourseArray.push(getPageAsync(baseUrl + id))
+  fetchCourseArray.push(getPageDataAsync(baseUrl + id))
 })
+
+/**
+Promise.all 可以接收一个元素为 Promise 对象的数组作为参数，
+当这个数组里面所有的 Promise 对象都变为 resolve 时，该方法才会返回。
+Promise.all 方法会按照数组里面的顺序将结果返回。
+还有一个和 Promise.all 相类似的方法 Promise.race，它同样接收一个数组，
+不同的是只要该数组中的 Promise 对象的状态发生变化（无论是 resolve 还是 reject）
+该方法都会返回。
+*/
+Promise
+  .all(fetchCourseArray)
+  .then(function(pages) {
+    var coursesData = []
+    pages.forEach(function(html) {
+      var course = filterChapters(html)
+
+      coursesData.push(course)
+    })
+
+    // courseData.sort(function(a, b) {
+    //   retrun a.number < b.number
+    // })
+
+    printCourseInfo(coursesData)
+  })
